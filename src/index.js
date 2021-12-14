498bb0cd540085cd4a2a2449b1d29db852bca89d
Dylan Sicard <dylan.sicard@gmail.com>
2021-12-13 20:08:18 -0600

    added delete functionality

--- src/index.js
+++ src/index.js
@@ -4,28 +4,7 @@ import ReactDOM from 'react-dom';
 
 const App = () => {
   const [value, setValue] = useState('');
-  const [todos, setTodos] = useState([
-    {
-      text: 'learn react',
-      isCompleted: false,
-    },
-    {
-      text: 'get programming job',
-      isCompleted: false,
-    },
-    {
-      text: 'live the dream',
-      isCompleted: false,
-    },
-  ]);
-
-  const renderTodos = todos.map((todo, i) => {
-    return (
-      <div className="todo" key={i}>
-        {todo.text}
-      </div>
-    );
-  });
+  const [todos, setTodos] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -33,6 +12,28 @@ const App = () => {
     setTodos([...todos, { text: value, isCompleted: false }]);
     setValue('');
   };
+
+  const handleDelete = (e) => {
+    const index = Number(e.target.id);
+    console.log(index);
+    let temp = [...todos];
+    console.log(temp);
+    temp.splice(index, 1);
+    setTodos(temp);
+  };
+
+  const renderTodos = todos.map((todo, i) => {
+    return (
+      <div className="todo" key={i} id={i}>
+        {todo.text}{' '}
+        <button id={i} onClick={handleDelete} className="ui button">
+          <div className="content">
+            <i className="trash alternate icon"></i>
+          </div>
+        </button>
+      </div>
+    );
+  });
 
   return (
     <React.Fragment>
