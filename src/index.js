06a901b73db0d27ce0b5da2ed963f13aab82a357
Dylan Sicard <dylan.sicard@gmail.com>
2021-12-13 22:44:45 -0600

    refactored form to component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import './index.css';
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import TodoForm from './TodoForm';
 
 const App = () => {
-  const [value, setValue] = useState('');
   const [todos, setTodos] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (!value) return;
-    setTodos([...todos, { text: value, isCompleted: false }]);
-    setValue('');
+  const addTodo = (text) => {
+    setTodos([...todos, { text: text, isCompleted: false }]);
   };
 
   const handleDelete = (e) => {
@@ -25,7 +22,14 @@ const App = () => {
   const renderTodos = todos.map((todo, i) => {
     return (
       <div className="todo" key={i} id={i}>
-        {todo.text}{' '}
+        <div class="ui left floated compact segment">
+          <div class="fitted checkbox">
+            <input type="checkbox" />
+            <label></label>
+          </div>
+        </div>
+        <div>{todo.text}</div>
+
         <button id={i} onClick={handleDelete} className="ui button">
           <div className="content">
             <i className="trash alternate icon"></i>
@@ -36,22 +40,15 @@ const App = () => {
   });
 
   return (
-    <React.Fragment>
+    <div className="app">
       <div>
         <h1>ToDos</h1>
       </div>
-      <div>{renderTodos}</div>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name=""
-          className="input"
-          value={value}
-          placeholder="Add Todo ..."
-          onChange={(e) => setValue(e.target.value)}
-        />
-      </form>
-    </React.Fragment>
+      <div className="todo-list">
+        {renderTodos}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
   );
 };
 
